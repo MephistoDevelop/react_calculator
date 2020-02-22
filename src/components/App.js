9bd0ac68cf4e7101b048a46cc08a94015c5cccd8
MephistoDevelop <inesherald@gmail.com>
2020-02-22 10:40:50 -0600

    Save changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,28 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from './logic/calculate';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: 0,
+      operarion: null,
+    };
+  }
+
+  this.handleClick = this.handleClick.bind(this);
+
+  handleClick(ButtonName){
+    this.setState(state => Calculate(state,ButtonName));
+  }
   render() {
     return (
       <div id="calculator-container">
         <Display result="10" />
-        <ButtonPanel />
+        <ButtonPanel clickHandler={this.handleClick}/>
       </div>
     );
   }
---
+++ src/components/logic/calculate.js
@@ -0,0 +1,5 @@
+const Calculate = (calculator, ButtonName) => {
+
+};
+
+export default Calculate;
